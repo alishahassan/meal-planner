6e36d9de4c676d60d2743409ae9103a127e12e79
NaomiYokoo <nlyokoo@g.syr.edu>
2025-12-14 15:43:51 -0500

    Revert "moved all images to img"
    This reverts commit 26e346f6c995c948ee6abe4377e82b86809fdff1.

--- tracking.js
+++ tracking.js
@@ -48,31 +48,31 @@ class Recipe {
 const chicken = new Recipe ({
     name: "Chicken Stir Fry",
     ingredients: ["chicken", "sauce", "egg"],
-    image: "../img/chicken2.jpg"
+    image: "../images/chicken2.jpg"
 })
 
 const toast = new Recipe ({
     name: "Toast",
     ingredients: ["bread", "butter"],
-    image: "../img/toast.jpg"
+    image: "../images/toast.jpg"
 })
 
 const hotdog = new Recipe ({
     name: "Hot Dog",
     ingredients: ["hotdog", "bun"],
-    image: "../img/hotdog.webp"
+    image: "../images/hotdog.webp"
 })
 
 const hotdog2 = new Recipe ({
     name: "Hot Dog",
     ingredients: ["hotdog", "bun"],
-    image: "../img/hotdog.webp"
+    image: "../images/hotdog.webp"
 })
 
 const hotdog3 = new Recipe ({
     name: "Hot Dog",
     ingredients: ["hotdog", "bun"],
-    image: "../img/hotdog.webp"
+    image: "../images/hotdog.webp"
 })
 
 let pastRecipes = [chicken, toast, hotdog];
